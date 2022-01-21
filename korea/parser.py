8260bb6b7beb199ecfdfcd338bf4f362ce316b15
HARASEON <82445403+HARASEON@users.noreply.github.com>
2022-01-21 10:04:16 +0900

    Update parser.py

--- korea/parser.py
+++ korea/parser.py
@@ -7,16 +7,10 @@ def parser_company(name):
     content = open("data/{}".format(name), 'r', encoding = 'utf-8-sig')
     json_data = json.load(content)['comp'] # comp에 해당하는 value값 불러오기
     for e in json_data:
-        print("name:{} share_rate:{}".format(e['SHER_NM'], e['SHER_RT_SUM']))
+        print("name:{} share_rate:{}".format(e['SHER_NM'], e['SHER_RT']))
 
 if __name__ == "__main__":
     company_list = os.listdir("data") 
-    print(company_list)
-
-    import pdb
-    pdb.set_trace()
-
     for c in company_list:
+        print("company name",c)
         parser_company(c)
-        import pdb
-        pdb.set_trace()
